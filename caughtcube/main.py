ca27e92d526403b05b7827fd5c2db4ef1d718eae
Jonathan Hartley <tartley@tartley.com>
2010-08-23 22:41:22 +0100

    move command line options into new module options.py

--- caughtcube/main.py
+++ caughtcube/main.py
@@ -1,23 +1,8 @@
 from __future__ import division
-
 import sys
 
-from . import version
 from .gameloop import Gameloop
-
-
-class Options(object):
-
-    def __init__(self, argv):
-        self.vsync = '--vsync' in argv
-        self.fullscreen = '--fullscreen' in argv or '-f' in argv
-        self.print_fps = '--print-fps' in argv
-        self.display_fps = '--fps' in argv
-        self.version = '--version' in argv
-
-        if self.version:
-            print version
-            sys.exit()
+from .options import Options
 
 
 def run_game():
