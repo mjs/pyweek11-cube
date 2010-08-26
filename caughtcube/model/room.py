c2133a26fe5a106d88ccf1ebe2763ef8e59da3c7
Jonathan Hartley <tartley@tartley.com>
2010-08-27 00:10:29 +0100

    walls edges now lie on integer ordinates. room uses same shape factory as wall

--- caughtcube/model/room.py
+++ caughtcube/model/room.py
@@ -1,38 +1,15 @@
 from __future__ import division
+from itertools import product
 
-from ..model.gameitem import GameItem
-from ..model.shape import Shape
-from ..util.color import all_colors
-
-
-def RoomShape(xmax, ymax, zmax, colors):
-    xmin, ymin, zmin = (-0.5, -0.5, -0.5)
-    xmax -= 0.5
-    ymax -= 0.5
-    zmax -= 0.5
-    verts = [
-        (xmin, ymin, zmin),
-        (xmin, ymin, zmax),
-        (xmin, ymax, zmin),
-        (xmin, ymax, zmax),
-        (xmax, ymin, zmin),
-        (xmax, ymin, zmax),
-        (xmax, ymax, zmin),
-        (xmax, ymax, zmax),
-    ]
-    faces = [
-        [2, 3, 1, 0], # 
-        [5, 7, 6, 4], # 
-        [5, 1, 3, 7], # 
-        [4, 6, 2, 0], # 
-        [5, 4, 0, 1], # -y bottom
-        [2, 6, 7, 3], # +y top        
-    ]
-    return Shape(verts, faces, colors)
+from ..util.color import white, blue
+from .gameitem import GameItem
+from .shape import Shape
+from .wall import WallShape
 
 
 def Room(xsize, ysize, zsize):
+    paleblue = white.tinted(blue, 0.1)
     return GameItem(
-        shape=RoomShape(xsize, ysize, zsize, all_colors),
+        shape=WallShape((xsize, ysize, zsize), paleblue, invert=True),
     )
 
