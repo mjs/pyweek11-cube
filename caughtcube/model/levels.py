161da502e9a0be8c4dfff538bd6d21b62702c158
Jonathan Hartley <tartley@tartley.com>
2010-08-27 00:38:03 +0100

    rearrange 1st level

--- caughtcube/model/levels.py
+++ caughtcube/model/levels.py
@@ -7,15 +7,17 @@ from .wall import Wall
 def populate(world, number):
     for item in levels[number]:
         world.add(item)
-    start_position = (1, 0, 1)
+    start_position = (2, 0, 2)
     return start_position
 
 
 levels = {
     1: [
         Room(8, 16, 8),
-        Wall((1, 2, 1), (4, 1, 2)),
-        Wall((1, 3, 4), (2, 0, 4)),
+        Wall((4, 1, 1), (0, 1, 1)),
+        Wall((1, 5, 1), (1, 0, 1)),
+        Wall((1, 1, 6), (1, 1, 0)),
+        Wall((2, 3, 4), (6, 0, 0)),
         Exit((3, 3, 3)),
     ],
 }
