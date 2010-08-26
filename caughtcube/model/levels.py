970bfbd5d7ec4466cd55cb1632a4d6c357b4a590
Jonathan Hartley <tartley@tartley.com>
2010-08-26 23:15:02 +0100

    room walls now aligh with integer boundaries

--- caughtcube/model/levels.py
+++ caughtcube/model/levels.py
@@ -1,10 +1,22 @@
+
 from .exit import Exit
+from .room import Room
 from .wall import Wall
 
-levels = [
-    [
+
+def populate(world, number):
+    for item in levels[number]:
+        world.add(item)
+    start_position = (1, 0, 1)
+    return start_position
+
+
+levels = {
+    1: [
+        Room(8, 16, 8),
         Wall((1, 2, 1), (-2, 0, -4)),
         Wall((1, 3, 4), ( 2, 0, -2)),
         Exit((3, 3, 3)),
     ],
-]
+}
+
--- caughtcube/model/room.py
+++ caughtcube/model/room.py
@@ -5,30 +5,35 @@ from ..model.shape import Shape
 from ..util.color import all_colors
 
 
-def RoomShape(edge, colors):
-    e = edge / 2
+def RoomShape(xmax, ymax, zmax, colors):
+    xmin, ymin, zmin = (-0.5, -0.5, -0.5)
+    xmax -= 0.5
+    ymax -= 0.5
+    zmax -= 0.5
     verts = [
-        (-e/2 - 0.5,        0.5, -e/2 - 0.5,),
-        (-e/2 - 0.5,        0.5, +e/2 + 0.5,),
-        (-e/2 - 0.5, +e/2 + 0.5, -e/2 - 0.5,),
-        (-e/2 - 0.5, +e/2 + 0.5, +e/2 + 0.5,),
-        (+e/2 + 0.5,        0.5, -e/2 - 0.5,),
-        (+e/2 + 0.5,        0.5, +e/2 + 0.5,),
-        (+e/2 + 0.5, +e/2 + 0.5, -e/2 - 0.5,),
-        (+e/2 + 0.5, +e/2 + 0.5, +e/2 + 0.5,),
+        (xmin, ymin, zmin),
+        (xmin, ymin, zmax),
+        (xmin, ymax, zmin),
+        (xmin, ymax, zmax),
+        (xmax, ymin, zmin),
+        (xmax, ymin, zmax),
+        (xmax, ymax, zmin),
+        (xmax, ymax, zmax),
     ]
     faces = [
-        [2, 3, 1, 0],
-        [5, 7, 6, 4],
-        [5, 1, 3, 7],
-        [4, 6, 2, 0],
-        [5, 4, 0, 1]
+        [2, 3, 1, 0], # 
+        [5, 7, 6, 4], # 
+        [5, 1, 3, 7], # 
+        [4, 6, 2, 0], # 
+        [5, 4, 0, 1], # -y bottom
+        [2, 6, 7, 3], # +y top        
     ]
     return Shape(verts, faces, colors)
 
 
-def Room(width):
+def Room(xsize, ysize, zsize):
     return GameItem(
-        shape=RoomShape(width, all_colors),
+        shape=RoomShape(xsize, ysize, zsize, all_colors),
+        
     )
 
