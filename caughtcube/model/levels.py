cc192572a05880ad6d0903bc42ccd260b91ee94e
Jonathan Hartley <tartley@tartley.com>
2010-08-27 09:36:56 +0100

    Rearrange level 1 to make it more maze-like Walls are now passed a color. Make our walls a similar color to the Room

--- caughtcube/model/levels.py
+++ caughtcube/model/levels.py
@@ -1,4 +1,5 @@
 
+from ..util.color import white, cyan
 from .exit import Exit
 from .room import Room
 from .wall import Wall
@@ -7,22 +8,26 @@ from .wall import Wall
 def populate(world, number):
     for item in levels[number]:
         world.add(item)
-    start_position = (0, 0, 0)
+    start_position = (5, 0, 0)
     return start_position
 
+
+paleblue = white.tinted(cyan, 0.15)
 
 levels = {
     1: [
         Room(8, 16, 8),
-        Wall((1, 1, 7), (2, 0, 1)),
-        Wall((1, 1, 9), (2, 1, 0)),
-        Wall((1, 1, 5), (4, 0, 0)),
-        Wall((1, 1, 5), (4, 0, 6)),
-        Wall((1, 1, 9), (4, 1, 0)),
-        Wall((1, 1, 9), (6, 1, 0)),
-        Wall((1, 1, 1), (6, 0, 7)),
-        Wall((1, 1, 6), (6, 0, 0)),
-        Exit((6, 0, 6)),
+        Wall((1, 1, 1), (6, 0, 0), paleblue),
+        Wall((6, 1, 1), (1, 0, 1), paleblue),
+        Wall((3, 1, 1), (0, 0, 3), paleblue),
+        Wall((1, 1, 4), (4, 0, 2), paleblue),
+        Wall((2, 1, 1), (6, 0, 3), paleblue),
+        Wall((5, 1, 1), (2, 0, 5), paleblue),
+        Wall((1, 1, 1), (0, 0, 4), paleblue),
+        Wall((2, 1, 1), (1, 0, 6), paleblue),
+        Wall((1, 1, 1), (6, 0, 6), paleblue),
+        Wall((1, 1, 1), (4, 0, 7), paleblue),
+        Exit((7, 0, -1)),
     ],
 }
 
