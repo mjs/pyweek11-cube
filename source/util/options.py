7120fd69bec0d65da37c4e31d70f77fdbe5f6aac
Jonathan Hartley <tartley@tartley.com>
2010-09-11 22:49:18 +0100

    fullscreen by default@

--- source/util/options.py
+++ source/util/options.py
@@ -7,7 +7,7 @@ class Options(object):
 
     def __init__(self, argv):
         self.novsync = '--novsync' in argv
-        self.fullscreen = '--fullscreen' in argv or '-f' in argv
+        self.fullscreen = '--window' not in argv and '-w' not in argv
         self.print_fps = '--print-fps' in argv
         self.display_fps = '--fps' in argv
         self.version = '--version' in argv
